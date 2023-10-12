4c7158d37494e3146ccf042891adc5888e32aac2
Bhavinrathava <bhavinr9@gmail.com>
2023-10-12 11:39:10 -0500

    Implemented Random Forest

--- ID3.py
+++ ID3.py
@@ -4,8 +4,8 @@ from parse import parse
 import utility
 import random
 import matplotlib.pyplot as plt 
-
-
+from RandomForest import RandomForest
+from collections import Counter
 
 def impute_missing_value(dataset):
     attributes = list(dataset[0].keys())
@@ -162,6 +162,22 @@ def evaluate(node, example):
             return None  # Handle leaf nodes with no attribute
 
 
+def randomForest(examples, trainSize, numTrees, default):
+    
+    randomForest = RandomForest()
+
+    for i in range(numTrees):
+       
+      random.shuffle(examples)
+      sliceDataset = examples[:trainSize]
+
+      tree = ID3(sliceDataset, default)
+      randomForest.trees.append(tree)
+
+    #Now we have our random forest complete
+    
+    return randomForest
+ 
 
 def generateTrainingGraph(examples):
   
@@ -199,9 +215,59 @@ def generateTrainingGraph(examples):
   plt.legend()
   plt.show()
 
+def EvaluateRandomForest(randomForest, testDataset):
+  correctCount = 0
+  for ex in testDataset:
+    values = []
+       
+    for currentTree in randomForest.trees:
+        values.append(evaluate(currentTree, ex))
+
+    
+    count = Counter(values)
+    RFDecision = max(count, key=count.get)
+
+    if(RFDecision == ex["Class"]):
+        correctCount +=1
+    
+  return correctCount / len(testDataset) 
+
+def compareRFDecisionTree(dataset, trainingSize, treeCount, default):
+  random.shuffle(dataset)
+  examples = dataset[:(9*len(dataset))//10]
+  testDataset = dataset[(9*len(dataset))//10:]
+
+  tree = ID3(examples, default)
+  treeAcc = test(tree, testDataset)
+  rdmForest = randomForest(examples, trainingSize, treeCount, default)
+  forestAcc = EvaluateRandomForest(rdmForest, testDataset)
+
+  return forestAcc, (forestAcc - treeAcc)/treeAcc * 100 
+
 if __name__ == "__main__":
   examples = parse("house_votes_84.data")
   examples = impute_missing_value(examples)
   # examples = utility.removeMissingValues(examples)
+  candy = parse("candy.data")
 
-  generateTrainingGraph(examples)
+  samplesize = [10,20,30,40,50]
+  treeCounts = [1,2,3,4,5,6,7,8,9,10]
+
+  bestSample = 10
+  bestCount = 1
+  bestAcc = 0
+  changeAccuracy = 0
+  for s in samplesize:
+     for t in treeCounts:
+      print("Test for sample size : {} and number of Trees in Random Forest :{}".format(s, t))
+      rfAccuracy, changeinAccuracy = compareRFDecisionTree(candy, s, t, 0)
+      if(rfAccuracy > bestAcc):
+         bestSample = s
+         bestCount = t
+         changeAccuracy = changeinAccuracy
+         bestAcc = rfAccuracy
+  
+  print("Based on experiment, we have concluded that RF with {} sample size and {} trees performs the best with {} accuracy which was {} better than Tree".format(bestSample, bestCount, bestAcc, changeAccuracy))
+
+  #generateTrainingGraph(examples)
+
